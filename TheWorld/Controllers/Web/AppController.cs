060d7ca523549e22d1859345568aa443eec212b9
Sonia Kurtz <kurtzsonia@gmail.com>
2017-04-23 18:58:38 +0200

    Contact Page implemented, validation, POST, services

--- TheWorld/Controllers/Web/AppController.cs
+++ TheWorld/Controllers/Web/AppController.cs
@@ -1,13 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TheWorld.Services;
+using TheWorld.ViewModels;
 
 namespace TheWorld.Controllers.Web
 {
     public class AppController : Controller
     {
+		private IMailService _mailService;
+		private IConfigurationRoot _config;
+
+		public AppController(IMailService mailService, IConfigurationRoot config)
+		{
+			_mailService = mailService;
+			_config = config;
+		}
+
 		public IActionResult Index ()
 		{
 			return View();
@@ -15,6 +27,20 @@ namespace TheWorld.Controllers.Web
 
 		public IActionResult Contact()
 		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Contact (ContactViewModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				_mailService.SendMail(_config["MailSetting:ToAddress"], model.Email, "From TheWorld", model.Message);
+
+				ModelState.Clear();
+
+				ViewBag.UserMessage = "Message sent";
+			}
 			return View();
 		}
 
